209aaeb4faec483419c428b619b20ab30740eabf
Ernests <austrins.ernests@gmail.com>
2024-07-27 16:26:08 +0300

    Ceasers cypher

---
+++ test.rb
@@ -0,0 +1,15 @@
+def caesar_cipher(text, shift)
+  alphabet  = Array('a'..'z')
+  non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  alphabet = Array('A'..'Z')
+  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  encoder = non_caps.merge(caps)
+  
+  encoded_text = ""
+  text.chars.each { |c| encoded_text += encoder.fetch(c, c) }
+  encoded_text
+end
+
+puts caesar_cipher("What a string!", 5)
